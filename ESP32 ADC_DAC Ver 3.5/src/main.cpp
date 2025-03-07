a9cfe29fa358b01ab3fed91f427d537602e05b81
Terra <130888420+TerrieKnight@users.noreply.github.com>
2025-03-07 11:29:01 -0500

    Version 5.0.cpp
    Full functionality of Square Wave function simulated, tested, and verified., although it may need fine tuning on its vref once full testing begins

--- ESP32 ADC_DAC Ver 3.5/src/main.cpp
+++ ESP32 ADC_DAC Ver 3.5/src/main.cpp
@@ -1,4 +1,4 @@
-// ESP32 ADC/DAC Version 4.5 //
+// ESP32 ADC/DAC Version 5.0 //
 
 // bring in required libraries 
 #include <Arduino.h>
@@ -10,18 +10,12 @@
 // initialize variables
 static esp_adc_cal_characteristics_t adc1_chars;
 float pitch_val; 
-int led_state = LOW;
 boolean SqaureWave_State = false; 
 
 // SQAURE WAVE FUNCTION //
-float SqaureWave(int led_state, float signal){
-  // control LED arccoding to the toggleed sate
-  digitalWrite(0, led_state);
-
-  // convert current sample to high or low based on treshold
-  signal = (signal > 128) ? 255 : 0;  
-
-  // return current sample to primary code 
+float SqaureWave(float signal){
+  // convert current sample to high or low based on treshold and return
+  signal = (signal > 64) ? 255 : 0;  
   return signal;
 }// end sqaure function 
 
@@ -30,14 +24,12 @@ ezButton RedButton(16);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("ESP32 ADC/DAC Version 4.5");
+  Serial.println("ESP32 ADC/DAC Version 5.0");
   // ADC characteristics, channel 1 ADC, Attenuated to max 3.9 V,  12 bit precision
   esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_12, ADC_WIDTH_BIT_12, 0, &adc1_chars);
   // ADC attenuation for pin 33
   adc1_config_channel_atten(ADC1_CHANNEL_5, ADC_ATTEN_DB_12); 
   dac_output_enable(DAC_CHANNEL_2);
-  // set pin 0 to output mode to power test led
-  pinMode(0, OUTPUT);   
   // set debounce time to 50 milliseconds for red button
   RedButton.setDebounceTime(50); 
 }// end void set up 
@@ -55,16 +47,9 @@ void loop() {
   }
 
   if (SqaureWave_State){
-    pitch_val = SqaureWave(HIGH, pitch_val);
-  } else {
-    led_state = LOW;
-    digitalWrite(0, led_state);
-  }
+    pitch_val = SqaureWave(pitch_val);
+  } 
 
   // Output corresponding analogue value, one shot mode
   dac_output_voltage(DAC_CHANNEL_2, pitch_val);
-
-  //Serial.print("Pitch : "); 
-  //Serial.println(pitch_val); 
-  //delayMicroseconds(25);
 }// end main loop
